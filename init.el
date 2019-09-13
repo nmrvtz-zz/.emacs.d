b9496f21d0b4171309636d134a484fafb62ae0ac
Aleksei Nemerovets <aleksei.nemerovets@omio.com>
2019-09-13 16:04:49 +0200

    Javascript environment

---
+++ init.el
@@ -0,0 +1,324 @@
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+(setq-default tab-width 2)
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Fira Code"
+                    :height 140
+                    :weight 'normal
+                    :width 'normal)
+
+(use-package diminish
+  :ensure t)
+
+(use-package better-defaults
+             :ensure t)
+
+(use-package exec-path-from-shell
+             :ensure t
+             :init
+             (setq exec-path-from-shell-check-startup-files nil
+                   exec-path-from-shell-variables '("PATH" "MANPATH" "PYTHONPATH" "GOPATH")
+                   exec-path-from-shell-arguments '("-l"))
+             (exec-path-from-shell-initialize))
+
+;; Editing
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package crux
+  :ensure t
+  :bind
+  ("C-k" . crux-smart-kill-line)
+  ("C-c n" . crux-cleanup-buffer-or-region)
+  ("C-c f" . crux-recentf-find-file)
+  ("C-a" . crux-move-beginning-of-line))
+
+(use-package avy
+  :ensure t
+  :bind
+  ("C-'" . avy-goto-char)
+  :config
+  (setq avy-background t))
+
+(use-package dtrt-indent
+  :ensure t
+  :commands dtrt-indent-mode
+  :hook (after-init . dtrt-indent-global-mode))
+
+; Themes
+;; (use-package base16-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'base16-nord t))
+
+(use-package kaolin-themes
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :init
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  :config
+  (doom-themes-visual-bell-config)
+  (load-theme 'doom-tomorrow-night t))
+
+(use-package smart-mode-line
+  :ensure t
+  :config
+  (setq sml/theme 'respectful)
+  (setq sml/no-confirm-load-theme t)
+  (sml/setup))
+
+;; Helm
+(use-package helm
+  :ensure t
+  :bind (("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-mini))
+         ;; ("C-x b" . helm-buffers-list))
+  :config
+  (require 'helm-config)
+  (setq helm-split-window-inside-p t
+    helm-move-to-line-cycle-in-source t)
+  (setq helm-autoresize-max-height 0)
+  (setq helm-autoresize-min-height 20)
+  (setq helm-command-prefix-key "C-c h")
+  (setq helm-M-x-fuzzy-match t)
+  (setq helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match    t)
+  (setq helm-semantic-fuzzy-match t
+        helm-imenu-fuzzy-match    t)
+  (setq helm-locate-fuzzy-match t)
+  (setq helm-apropos-fuzzy-match t)
+  (setq helm-lisp-fuzzy-completion t)
+  (setq helm-mode-fuzzy-match t)
+  (setq helm-completion-in-region-fuzzy-match t)
+  (setq helm-candidate-number-list 50)
+  (setq projectile-completion-system 'helm)
+  (helm-autoresize-mode 1)
+  (helm-mode 1))
+
+(use-package helm-descbinds
+  :ensure t
+  :config
+  (helm-descbinds-mode))
+
+(use-package helm-projectile
+  :ensure t
+  :config
+  (helm-projectile-on))
+
+(use-package helm-ag
+  :ensure t)
+
+;; Projectile
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :bind (("C-c p f" . helm-projectile-find-file)
+         ("C-c p p" . helm-projectile-switch-project)
+         ("C-c p s" . projectile-save-project-buffers))
+  :config
+  (projectile-global-mode))
+
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :diminish whick-key-mode
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+;; Javascript
+
+(use-package js2-mode
+             :ensure t
+             :config
+             (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+
+(use-package company
+  :diminish company-mode
+  :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
+  :commands company-abort
+  :bind (("M-/" . company-complete)
+         ("<backtab>" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . my-company-yasnippet)
+         ;; ("C-c C-y" . my-company-yasnippet)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+  :hook (after-init . global-company-mode)
+  :init
+  (defun my-company-yasnippet ()
+    (interactive)
+    (company-abort)
+    (call-interactively 'company-yasnippet))
+  :config
+  (setq company-tooltip-align-annotations t
+        company-tooltip-limit 12
+        ;; company-idle-delay 0
+        company-echo-delay (if (display-graphic-p) nil 0)
+        company-minimum-prefix-length 2
+        company-require-match nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-downcase nil))
+
+(use-package company-lsp
+  :init (setq company-lsp-cache-candidates 'auto
+              company-lsp-async t))
+
+(use-package lsp-mode
+  :diminish lsp-mode
+  :ensure t
+  :hook (prog-mode . lsp-deferred)
+  :bind (:map lsp-mode-map
+              ("C-c C-d" . lsp-describe-thing-at-point))
+  :init (setq lsp-auto-guess-root t
+              lsp-prefer-flymake nil
+              lsp-enable-indentation nil
+              lsp-enable-on-type-formatting nil)
+  :commands lsp)
+
+(use-package flycheck
+  :ensure t
+  :diminish flycheck-mode
+  :config
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules))
+
+(use-package yasnippet
+  :ensure t)
+
+(use-package yasnippet-snippets
+  :ensure t)
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)
+         ("C-x M-g" . magit-dispatch)
+         ("C-c M-g" . magit-file-popup))
+  :config
+  ;; (setq eieio-backward-compatibility nil)
+
+  (if (fboundp 'transient-append-suffix)
+      ;; Add switch: --tags
+      (transient-append-suffix 'magit-fetch
+        "-p" '("-t" "Fetch all tags" ("-t" "--tags"))))
+
+  ;; Access Git forges from Magit
+  (when (executable-find "cc")
+    (use-package forge
+      :ensure t
+      :demand)))
+
+(use-package diff-hl
+  :ensure t
+  :defines (diff-hl-margin-symbols-alist desktop-minor-mode-table)
+  :commands diff-hl-magit-post-refresh
+  ;; :functions  my-diff-hl-fringe-bmp-function
+  :custom-face (diff-hl-change ((t (:foreground ,(face-background 'highlight)))))
+  :bind (:map diff-hl-command-map
+         ("SPC" . diff-hl-mark-hunk))
+  :hook ((after-init . global-diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode))
+  :config
+  ;; Highlight on-the-fly
+  (diff-hl-flydiff-mode 1)
+
+  ;; Set fringe style
+  (setq-default fringes-outside-margins t)
+
+  ;; (defun my-diff-hl-fringe-bmp-function (_type _pos)
+  ;;   "Fringe bitmap function for use as `diff-hl-fringe-bmp-function'."
+  ;;   (define-fringe-bitmap 'my-diff-hl-bmp
+  ;;     (vector (if sys/macp #b11100000 #b11111100))
+  ;;     1 8
+  ;;     '(center t)))
+  ;; (setq diff-hl-fringe-bmp-function #'my-diff-hl-fringe-bmp-function)
+
+  (unless (display-graphic-p)
+    (setq diff-hl-margin-symbols-alist
+          '((insert . " ") (delete . " ") (change . " ")
+            (unknown . " ") (ignored . " ")))
+    ;; Fall back to the display margin since the fringe is unavailable in tty
+    (diff-hl-margin-mode 1)
+    ;; Avoid restoring `diff-hl-margin-mode'
+    (with-eval-after-load 'desktop
+      (add-to-list 'desktop-minor-mode-table
+                   '(diff-hl-margin-mode nil))))
+
+  ;; Integration with magit
+  (with-eval-after-load 'magit
+    (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh)))
+
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "0d087b2853473609d9efd2e9fbeac088e89f36718c4a4c89c568dd1b628eae41" "001c2ff8afde9c3e707a2eb3e810a0a36fb2b466e96377ac95968e7f8930a7c5" "0fe9f7a04e7a00ad99ecacc875c8ccb4153204e29d3e57e9669691e6ed8340ce" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "d6f04b6c269500d8a38f3fabadc1caa3c8fdf46e7e63ee15605af75a09d5441e" "5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "2878517f049b28342d7a360fd3f4b227086c4be8f8409f32e0f234d129cee925" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" "f589e634c9ff738341823a5a58fc200341b440611aaa8e0189df85b44533692b" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "93268bf5365f22c685550a3cbb8c687a1211e827edc76ce7be3c4bd764054bad" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "8c1dd3d6fdfb2bee6b8f05d13d167f200befe1712d0abfdc47bb6d3b706c3434" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "1025e775a6d93981454680ddef169b6c51cc14cea8cb02d1872f9d3ce7a1da66" "808b47c5c5583b5e439d8532da736b5e6b0552f6e89f8dafaab5631aace601dd" "80930c775cef2a97f2305bae6737a1c736079fdcc62a6fdf7b55de669fbbcd13" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
+ '(package-selected-packages
+   (quote
+    (diminish yasnippet-snippets which-key use-package smartparens smart-mode-line-powerline-theme smart-mode-line-atom-one-dark-theme js2-mode helm-projectile helm-descbinds helm-ag forge expand-region exec-path-from-shell dtrt-indent doom-themes diff-hl crux company-lsp company-dcd better-defaults base16-theme avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:foreground "#81a2be")))))
