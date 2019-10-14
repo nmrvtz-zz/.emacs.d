88685b4635ba946d4d20c79a477ad088ceed45ea
Aleksei Nemerovets <aleksei.nemerovets@omio.com>
2019-10-14 16:38:40 +0200

    Web mode

--- init.el
+++ init.el
@@ -393,6 +393,11 @@
 (use-package typescript-mode
   :ensure t)
 
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode)))
+
 ;; Clojure
 (use-package clojure-mode
   :ensure t)
